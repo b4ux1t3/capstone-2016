c82b0e25bbf4c50b6cd2f5c7ae9c0678ffc7a704
Erica Blinder <eblinder88@gmail.com>
2016-02-11 21:12:44 -0500

    Created GUIs: Patient_Consultation, Patient_ConsultTreatments, and Reporting_Home

--- application/capstone/capstone/Reporting_Home.xaml.cs
+++ application/capstone/capstone/Reporting_Home.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace capstone
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window3.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window3 : Window
     {
-        public MainWindow()
+        public Window3()
         {
             InitializeComponent();
-        }
-
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-        
         }
     }
 }
