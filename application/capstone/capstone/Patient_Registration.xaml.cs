6760da832693a58aa67627dd7b6e6a524275cc6c
Erica Blinder <eblinder88@gmail.com>
2016-02-15 18:14:52 -0500

    Completed GUIs: Appointments, Patient_Home, Patient_Lookup, Pateint_Registration

---
+++ application/capstone/capstone/Patient_Registration.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace capstone
+{
+    /// <summary>
+    /// Interaction logic for Window5.xaml
+    /// </summary>
+    public partial class Window5 : Window
+    {
+        public Window5()
+        {
+            InitializeComponent();
+        }
+
+        private void txtFirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtLastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtBDayMonth_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtBDayDay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtBDayYear_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void radbtnMale_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radbtnFemale_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtArea_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtSwitch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtLine_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtAddress_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void drpInsurance_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
