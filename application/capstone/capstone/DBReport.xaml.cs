d4065e830fc9f29da042671d3ecd6ed28bc68a2f
Chris Pilcher <Chris Pilcher>
2016-02-19 01:20:12 -0500

    Added some functionality to Medical Advice

---
+++ application/capstone/capstone/DBReport.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace capstone
+{
+    /// <summary>
+    /// Interaction logic for DBReport.xaml
+    /// </summary>
+    public partial class DBReport : Window
+    {
+        Window SourceWindow;
+        public DBReport(Window sourceWindow)
+        {
+            InitializeComponent();
+
+            this.SourceWindow = sourceWindow;
+        }
+
+        private void btnOkay_Click(object sender, RoutedEventArgs e)
+        {
+            SourceWindow.Show();
+            this.Close();
+        }
+    }
+}
